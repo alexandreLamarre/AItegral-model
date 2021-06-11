6a3f9d91c4a2da15ab29a18f951c78949eb246bd
alexandreLamarre <alex7285@gmail.com>
2021-06-10 18:38:40 -0800

    documented startup.pl

--- data/startup.pl
+++ data/startup.pl
@@ -1,3 +1,15 @@
-# this file is called when the docker container starts up!
+=head1 NAME
+    startup
+
+=head1 USAGE
+    perl startup.pl
+
+=head1 DESCRIPTION
+    This script is called when the developper docker container mounts.
+=cut
 
 print "Hello World!";
+
+=head1 Author
+    Alexandre Lamarre
+=cut
