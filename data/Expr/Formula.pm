1b1bdf3a7650650e602de6c469705b88e565aee1
alexandreLamarre <alex7285@gmail.com>
2021-06-22 18:00:47 -0400

    Formula package enhancements: symbolic functions and placeholders

--- data/Expr/Formula.pm
+++ data/Expr/Formula.pm
@@ -23,10 +23,21 @@ sub testLoad{
 =cut
 sub new {
     my $class = shift();
+    my $vars = shift();
+    my $consts = shift();
+    my $data = shift();
+    my $nesting = shift();
+    if(!defined($vars) || !defined($consts) || !defined($data)){
+        die "Expected defined vars,consts and data for Formula Object\n";
+    }
+    if(! defined($nesting)){
+        $nesting = 1;
+    }
     my $self = {
-        _vars => shift(),
-        _consts => shift(),
-        _data => shift()
+        _vars => $vars,
+        _consts => $consts,
+        _data => $data,
+        _nesting => $nesting
     };
 
     bless $self, $class;
@@ -35,7 +46,7 @@ sub new {
 
 =head1 getVars
     Method for formula object that returns the variables for the formula
-    @returns String: variables in the formula object
+    @returns <String>: variables in the formula object
 =cut
 sub getVars{
     my ($self) = @_;
@@ -44,7 +55,7 @@ sub getVars{
 
 =head1 getConsts
     Method for formula object that returns the constants for the formula
-    @returns String: constants in the formula object
+    @returns <String>: constants in the formula object
 =cut
 sub getConsts{
     my ($self) = @_;
@@ -53,11 +64,20 @@ sub getConsts{
 
 =head1 getData
     Method for formula object that returns the "data" for the formula
-    @returns String: the actual symbolic information of 
+    @returns <String>: the actual symbolic information of 
 =cut
 sub getData{
     my ($self) = @_;
     return $self -> {_data};
+}
+
+=head1 getNesting
+    Method for formula object that returns the nesting parameter of
+    the formula. Useful as a hyperparameter.
+=cut
+sub getNesting{
+    my ($self) = @_;
+    return $self -> {_nesting};
 }
 
 =head1 isValid
@@ -66,11 +86,11 @@ sub getData{
         - No two consecutive variable/constants
         - No alphabetical strings that don't match builtin functions
         - 
-    @returns bool : True if formula is valid OR False is formula is invalid
+    @returns <bool> : True if formula is valid OR False is formula is invalid
 =cut
 sub isValid{
     my ($self) = @_;
-    #TODO: IMPLEMENT
+    #TODO: IMPLEMENT using symbolic operator stacks
     return 0;
 }
 
@@ -82,21 +102,24 @@ sub isValid{
     @returns Array<String> : valid operators for symbolic formulas
 =cut
 sub getOperators{
-    return ("\+", "-", "\*", "/"); 
+    return ("\+", "-", "\*", "/"); # '^'' is excluded from this since it can also produce 
+                #complex functions, and should be considered a function not an operator 
 }
 
 =head1 getOperatorsRegex
     Returns the valid operators as regex matcher string
-    @returns regex String : 
+    @returns regex <String> : 
 =cut
 sub getOperatorsRegex{
-    return "(\+|-|\*|/)";
+    return "(\+|-|\*|/|\^)"; # $ is included here since we want to match valid operators, 
+                    # and it is difficult to integrate it with standardFuncsRegex subroutine.
 }
 
 =head1 getStandardFuncs
     Gets all standard mathematical functions that are used in integrals
     @param n int: nesting factor placeholder, starting at 1 
                   (For later use when generating random formulas)
+    @returns <Array> : contains all the symbolic elementary functions with placeholder '__vars$n__'
 =cut
 sub getStandardFuncs{
     my $n = shift();
@@ -104,18 +127,35 @@ sub getStandardFuncs{
         $n = 1;
     }
     my $p = "\_\_VAR$n\_\_"; 
-    #TODO: return array of standard functions
+    # (polynomials/exponentials, roots, sin, cos, tan, log,
+    #  arccos, arcsin, arctan, 
+    #  sec, csc, cot
+    #  arcsec, arccsc, arccot
+    #  sinh, cosh, tanh,
+    #  sech, cosh, tanh,
+    #  arcsinh, arccosh, arctanh,
+    #  arcsech, arccsch, arccoth)
+    my @funcs = ("$p\^\($p\)", "$p\^\(1\/$p\)", "sin\($p\)", "cos\($p\)", "tan\($p\)", "log\($p\)",
+                "arcsin\($p\)", "arccos\($p\)", "arctan\($p\)",
+                "sec\($p\)", "csc\($p\)", "cot\($p\)",
+                "arcsec\($p\)", "arccsc\($p\)", "arccot\($p\)",
+                "sinh\($p\)", "cosh\($p\)", "tanh\($p\)",
+                "sech\($p\)", "csch\($p\)", "coth\($p\)",
+                "arcsinh\($p\)", "arccosh\($p\)", "arctanh\($p\)",
+                "arcsech\($p\)", "arccsch\($p\)", "arccoth\($p\)");
 }
 
 =head1 getStandardFuncsRegex
-
+    Used for matching valid 'string' symbolic functions so it excludes ^ exponentionation operator. 
+    This operator is included in getOperatorsRegex
+    @returns regexMatching <string> : matches 'string' symbolic functions used in Aitegral
 =cut
 sub getStandardFuncsRegex{
-    #TODO: return regex matcher of standard functions
+    return "(sin|cos|tan|log|arcsin|arccos|arctan|sec|csc|cot|arcsec|arccsc|arccot|sinh|cosh|tanh|arcsinh|arccosh|arctanh|arcsech|arccsch|arccoth)";
 }
 
 1;
 
-=head1 Author
+=head1 Author(s)
     Alexandre Lamarre
 =cut
