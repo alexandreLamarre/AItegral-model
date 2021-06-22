d6abbcdf8cb53ffe6a80f61973ae816046aaed47
alexandreLamarre <alex7285@gmail.com>
2021-06-22 18:01:23 -0400

    Nearly complete test suites for Formula package

--- data/tests/formula.test.pl
+++ data/tests/formula.test.pl
@@ -4,28 +4,73 @@ use warnings;
 use FindBin;
 use Test;
 
-BEGIN{plan tests => 10, todo => []}
+BEGIN{plan tests => 13, todo => [13]}
 
 # relative imports from this directory
 use lib "$FindBin::RealBin/../Expr";
 use Formula;
 
+=head1 Test suite 0: Loading package
+    (1). Load the Formula package
+=cut
+
+=head1 Test suite 1: Symbolic operators static functions
+    (2)-(5). getOperators tests
+    (6). getOperatorsRegex test
+=cut
+
+=head1 Test suite 2: Formula object tests 
+    (7) Constructor test
+    (8)-(12). Getter method tests 
+    (13) Invalid constructor
+=cut
+
+=head1 Test suite 3: Symbolic elementary functions
+
+    (14)-(15). getStandardFuncsRegex test
+    (16) getStandardFuncs test
+
+=cut
+
+# ========== LOAD test ===========
+#(1)
 ok(Formula::testLoad(), 1);
 
-# ======= "static" function tests =============
-
+# =========== SYMBOLIC operator test =========
 my ($plus, $minus, $times, $div) = ("\+", "-", "\*", "/");
 my @operatorRes = Formula::getOperators();
 
-ok($operatorRes[0], $plus);
-ok($operatorRes[1], $minus);
-ok($operatorRes[2], $times);
-ok($operatorRes[3], $div);
+ok($operatorRes[0], $plus); #(2)
+ok($operatorRes[1], $minus); #(3)
+ok($operatorRes[2], $times); #(4)
+ok($operatorRes[3], $div); #(5)
+ok(Formula::getOperatorsRegex, "(".$plus."|".$minus."|".$times."|".$div."|\^)"); #(6)
 
-# ========== OOP tests =================
-ok(Formula::getOperatorsRegex, "(".$plus."|".$minus."|".$times."|".$div.")");
+# ========== Formula Object tests =================
+
 my $f = Formula::new Formula("ab","c","a*b+c");
-ok($f);
-ok($f->getVars, "ab");
-ok($f->getConsts, "c");
-ok($f->getData, "a*b+c");
+ok($f); #(7)
+ok($f->getVars, "ab"); #(8)
+ok($f->getConsts, "c"); #(9)
+ok($f->getData, "a*b+c"); #(10)
+ok($f->getNesting, 1); #(11)
+my$f1 = Formula::new Formula("ab", "c", "a*b+c", 3); #(12)
+ok($f1->getNesting, 3); #(13)
+
+
+my $passed = 1;
+my $funcsRegex = Formula::getStandardFuncsRegex();
+my @testCases = ("sin", "cos","log","arccos","arcsin","tan","arctan","sec","csc","cot","arcsec",
+                "arccsc","arccot","sinh","cosh","tanh","sech","csch","coth","arcsinh","arccosh",
+                "arctanh","arcsech","arccsch","arccoth)");
+foreach my $testVar (@testCases){
+    if(!$testVar =~ m/$funcsRegex/){
+        $passed = 0;
+        print "$testVar failed getStandardFuncsRegex match\n";
+    }
+}
+ok($passed, 1);
+
+ok(Formula::getStandardFuncs(14)); #TODO: make non lazy test for this
+
+
