fec48a52efd7939c83a2ff14173ee915ecd49195
alexandreLamarre <alex7285@gmail.com>
2021-06-21 21:38:53 -0400

    Formula package refactors & OOP functionality/static functions

--- data/Expr/Formula.pm
+++ data/Expr/Formula.pm
@@ -1,44 +1,121 @@
-package Expr::Formula;
-use Exporter 'import';
-our @EXPORT_OK = qw(new, getOperators);
-
-=head1 NAME
-
-    Expr::Formula - A module for encapsulating formula data
-
-=head1 SYNOPSIS
-
-    This module defines an Expr::Formula class for encapsulating
-    symbolic mathematical formulas as strings
-
-=cut
-
 =head1 Formula
 
-    Constructor for formula object:
-    @param 0: the class name
-    @param 1: the formula
+    Formula package : a package for encapsulating data and functions
+    related to symbolic formulas and their manipulation.
 
 =cut
+
+package Formula;
+# non relative imports from /usr/bin/perl
+use strict;
+use warnings;
+# relative imports from current directory
+
+sub testLoad{
+    print "Formula package successfully loaded!\n";
+    return 1;
+}
+
+=head1 Formula
+    Constructor for formula object:
+    @param class Object<Name>: the class name
+    @param data <String>: the formula
+=cut
 sub new {
-    my $class = @_[0];
+    my $class = shift();
     my $self = {
-        _data = @_[1];
+        _vars => shift(),
+        _consts => shift(),
+        _data => shift()
     };
 
-    bless $self $class;
+    bless $self, $class;
     return $self;
 }
 
+=head1 getVars
+    Method for formula object that returns the variables for the formula
+    @returns String: variables in the formula object
+=cut
+sub getVars{
+    my ($self) = @_;
+    return $self ->{_vars};
+}
+
+=head1 getConsts
+    Method for formula object that returns the constants for the formula
+    @returns String: constants in the formula object
+=cut
+sub getConsts{
+    my ($self) = @_;
+    return $self -> {_consts};
+}
+
+=head1 getData
+    Method for formula object that returns the "data" for the formula
+    @returns String: the actual symbolic information of 
+=cut
+sub getData{
+    my ($self) = @_;
+    return $self -> {_data};
+}
+
+=head1 isValid
+    Method for formula object that checks if the formula is indeed valid,
+    according to the following criteria:
+        - No two consecutive variable/constants
+        - No alphabetical strings that don't match builtin functions
+        - 
+    @returns bool : True if formula is valid OR False is formula is invalid
+=cut
+sub isValid{
+    my ($self) = @_;
+    #TODO: IMPLEMENT
+    return 0;
+}
+
+
+#=========== "static" package functions ==============
+
 =head1 getOperators
-
-    Returns the mathematical operators that can be
-    used in a formula.
-
+    returns valid operators for symbolic formulas
+    @returns Array<String> : valid operators for symbolic formulas
 =cut
 sub getOperators{
-    return ("+", "-", "/", "*"); 
+    return ("\+", "-", "\*", "/"); 
+}
+
+=head1 getOperatorsRegex
+    Returns the valid operators as regex matcher string
+    @returns regex String : 
+=cut
+sub getOperatorsRegex{
+    return "(\+|-|\*|/)";
+}
+
+=head1 getStandardFuncs
+    Gets all standard mathematical functions that are used in integrals
+    @param n int: nesting factor placeholder, starting at 1 
+                  (For later use when generating random formulas)
+=cut
+sub getStandardFuncs{
+    my $n = shift();
+    if(!defined($n) || $n <= 0){
+        $n = 1;
+    }
+    my $p = "\_\_VAR$n\_\_"; 
+    #TODO: return array of standard functions
+}
+
+=head1 getStandardFuncsRegex
+
+=cut
+sub getStandardFuncsRegex{
+    #TODO: return regex matcher of standard functions
 }
 
 1;
 
+=head1 Author
+    Alexandre Lamarre
+=cut
